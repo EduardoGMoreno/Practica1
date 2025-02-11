c6d3348ad39cec22bf4238a664c1ad86eea0fdff
Jesus <alu.21130575@correo.itlalaguna.edu.mx>
2025-02-10 21:12:49 -0600

    Clase 10 feb

--- app/src/main/java/com/example/ejemplo1b/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1b/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.ejemplo1b
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDirection.Companion.Content
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1b.ui.theme.Ejemplo1BTheme
@@ -24,7 +27,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1BTheme {
-                Content()
+                GreetingPreview()
             }
         }
     }
@@ -33,16 +36,18 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun Content() {
-    Text(
-        text = "Hola JetpackComposable",
-        color= Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .padding()
-            .background(Color.Yellow)
+fun GreetingPreview() {
+    Content(mensaje1 = "Hola", mensaje2 = "Mundo")
+}
 
-    )
+@Composable
+fun Content(mensaje1: String, mensaje2: String){
+    Column {
+        Text(mensaje1,
+            fontSize = 30.sp)
+        Text(mensaje2)
+        Text("Jesus",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold)
+           }
 }
