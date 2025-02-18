1b7d96805095e87b993612f4516d403a2586565c
Jesus <alu.21130575@correo.itlalaguna.edu.mx>
2025-02-18 15:08:50 -0600

    Practica Del dia 2 18-Febrero-2025

--- app/src/main/java/com/example/ejemplo1b/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1b/MainActivity.kt
@@ -4,17 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1b.ui.theme.Ejemplo1BTheme
 
@@ -24,7 +43,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1BTheme {
-                Content()
+                GreetingPreview()
             }
         }
     }
@@ -33,16 +52,66 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun Content() {
-    Text(
-        text = "Hola JetpackComposable",
-        color= Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
-        textAlign = TextAlign.Center,
+fun GreetingPreview() {
+    Column(
         modifier = Modifier
-            .padding()
-            .background(Color.Red)
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SimpleImage()
+        Spacer(modifier = Modifier.height(16.dp))
+        TextFieldWithInputType()
+        Spacer(modifier = Modifier.height(16.dp))
+        TextFieldWithInputType2()
+        Spacer(modifier = Modifier.height(16.dp))
+        SimpleButton()
+    }
+}
+
+@Composable
+fun SimpleImage() {
+    Image(
+        painter = painterResource(id = R.drawable.wpp),
+        contentDescription = "Andy Rubin",
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun TextFieldWithInputType() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        label = { Text(text = "Valor 1") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = { it ->
+            text = it
+        }
 
     )
+}
+
+@Composable
+fun TextFieldWithInputType2() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        label = { Text(text = "Valor 1") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = { it ->
+            text = it
+        }
+    )
+}
+@Composable
+fun SimpleButton() {
+    Button(onClick = {
+        //your onclick code here
+    }
+    ) {
+        Text(text = "Calcular ")
+    }
 }
